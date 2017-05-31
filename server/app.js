b3227554f40e38ddbc82d38a2daaf7d5a40a845e
Gus Escola <camagrock@gmail.com>
2017-05-31 12:30:01 +0200

    adding nav and transport bar

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const path = require ('path')
 const app = express()
 
-
+app.set('port', (process.env.PORT || 3000));
 //rutas estáticas
 
 app.use('/styles', express.static(path.join(__dirname, '../styles')))
@@ -12,13 +12,17 @@ app.use('/scripts', express.static(path.join(__dirname, '../scripts')))
 app.use('/samples', express.static(path.join(__dirname, '../samples')))
 app.use('/images', express.static(path.join(__dirname, '../images')))
 
-app.set('port', (process.env.PORT || 3000));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../index.html'));
-});
+
+app.use(express.static('public'));
+
 app.use(function (err, req, res, next) {
     console.error(err, err.stack);
     res.status(500).send(err);
 });
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(process.env.PORT || 3000)
