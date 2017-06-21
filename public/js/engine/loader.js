43efc94275d99b00bb7109b580ac86800e915383
Gus Escola <camagrock@gmail.com>
2017-06-21 19:33:58 +0200

    sessionStorage for drumMachine

--- public/js/engine/loader.js
+++ public/js/engine/loader.js
@@ -18,9 +18,7 @@ nx.onload = function() {
     notesMatrix.col = 8
     notesMatrix.colors.accent = "#ffc600"
     notesMatrix.init()
-    // masterMix.init()
-    // masterMix.colors.accent = "#A6A6A6"
-
+    
     var arpegio = [arpegioMatrix, arpegioMatrix2 ,arpegioMatrix3,arpegioMatrix4]
     arpegio.forEach(function(mat) {
         mat.col = 8
@@ -35,8 +33,144 @@ nx.onload = function() {
         mat.init()
         mat.colors.accent = "#ffc600"
     })
+    let four_kick = localStorage.getItem('four_kick');
+    let eight_kick = localStorage.getItem("eight_kick");
+    let triplet_kick = localStorage.getItem("triplet_kick");
+    let sixteen_kick = localStorage.getItem("sixteen_kick");
+    let four_snare = localStorage.getItem("four_snare");
+    let eight_snare = localStorage.getItem("eight_snare");
+    let triplet_snare = localStorage.getItem("triplet_snare");
+    let sixteen_snare = localStorage.getItem("sixteen_snare");
+    let four_hihat = localStorage.getItem("four_hihat");
+    let eight_hihat = localStorage.getItem("eight_hihat");
+    let triplet_hihat = localStorage.getItem("triplet_hihat");
+    let sixteen_hihat = localStorage.getItem("sixteen_hihat");
+    let four_fx = localStorage.getItem("four_fx");
+    let eight_fx = localStorage.getItem("eight_fx");
+    let triplet_fx = localStorage.getItem("triplet_fx");
+    let sixteen_fx = localStorage.getItem("sixteen_fx");
 
+    let parsed_four_kick = four_kick.split(",")
+    let parsed_eight_kick = eight_kick.split(",")
+    let parsed_triplet_kick = triplet_kick.split(",")
+    let parsed_sixteen_kick = sixteen_kick.split(",")
+    let parsed_four_snare = four_snare.split(",")
+    let parsed_eight_snare = eight_snare.split(",")
+    let parsed_triplet_snare = triplet_snare.split(",")
+    let parsed_sixteen_snare = sixteen_snare.split(",")
+    let parsed_four_hihat = four_hihat.split(",")
+    let parsed_eight_hihat = eight_hihat.split(",")
+    let parsed_triplet_hihat = triplet_hihat.split(",")
+    let parsed_sixteen_hihat = sixteen_hihat.split(",")
+    let parsed_four_fx = four_fx.split(",")
+    let parsed_eight_fx = eight_fx.split(",")
+    let parsed_triplet_fx = triplet_fx.split(",")
+    let parsed_sixteen_fx = sixteen_fx.split(",")
+
+    console.log(parsed_four_kick)
+    if(parsed_four_kick!== undefined ||parsed_four_kick !== null ||parsed_four_kick != undefined || parsed_four_kick != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        kickMatrix.matrix[i][0] = parseInt(parsed_four_kick[i]) 
+        kickMatrix.draw()
+        }
+    }
+     if(parsed_eight_kick!== undefined ||parsed_eight_kick !== null ||parsed_eight_kick != undefined || parsed_eight_kick != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        kickMatrix2.matrix[i][0] = parseInt(parsed_eight_kick[i]) 
+        kickMatrix2.draw()
+        }
+    }
+     if(parsed_triplet_kick!== undefined ||parsed_triplet_kick !== null ||parsed_triplet_kick != undefined || parsed_triplet_kick != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        kickMatrix3.matrix[i][0] = parseInt(parsed_triplet_kick[i]) 
+        kickMatrix3.draw()
+        }
+    }
+     if(parsed_sixteen_kick!== undefined ||parsed_sixteen_kick !== null ||parsed_sixteen_kick != undefined || parsed_sixteen_kick != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        kickMatrix4.matrix[i][0] = parseInt(parsed_sixteen_kick[i]) 
+        kickMatrix4.draw()
+        }
+    }
+     if(parsed_four_snare!== undefined ||parsed_four_snare !== null ||parsed_four_snare != undefined || parsed_four_snare != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        snareMatrix.matrix[i][0] = parseInt(parsed_four_snare[i]) 
+        snareMatrix.draw()
+        }
+    }
+     if(parsed_eight_snare!== undefined ||parsed_eight_snare !== null ||parsed_eight_snare != undefined || parsed_eight_snare != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        snareMatrix2.matrix[i][0] = parseInt(parsed_eight_snare[i]) 
+        snareMatrix2.draw()
+        }
+    }
+     if(parsed_triplet_snare!== undefined ||parsed_triplet_snare !== null ||parsed_triplet_snare != undefined || parsed_triplet_snare != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        snareMatrix3.matrix[i][0] = parseInt(parsed_triplet_snare[i]) 
+        snareMatrix3.draw()
+        }
+    }
+     if(parsed_sixteen_snare!== undefined ||parsed_sixteen_snare !== null ||parsed_sixteen_snare != undefined || parsed_sixteen_snare != null){
+        for(var i=0; i<= parsed_four_kick.length -1;i++){
+        snareMatrix4.matrix[i][0] = parseInt(parsed_sixteen_snare[i]) 
+        snareMatrix4.draw()
+        }
+    }
+     if(parsed_four_hihat!== undefined ||parsed_four_hihat !== null ||parsed_four_hihat != undefined || parsed_four_hihat != null){
+        for(var i=0; i<= parsed_four_hihat.length -1;i++){
+        hihatMatrix.matrix[i][0] = parseInt(parsed_four_hihat[i]) 
+        hihatMatrix.draw()
+        }
+    }
+     if(parsed_eight_hihat!== undefined ||parsed_eight_hihat !== null ||parsed_eight_hihat != undefined || parsed_eight_hihat != null){
+        for(var i=0; i<= parsed_eight_hihat.length -1;i++){
+        hihatMatrix2.matrix[i][0] = parseInt(parsed_eight_hihat[i]) 
+        hihatMatrix2.draw()
+        }
+    }
+     if(parsed_triplet_hihat!== undefined ||parsed_triplet_hihat !== null ||parsed_triplet_hihat != undefined || parsed_triplet_hihat != null){
+        for(var i=0; i<= parsed_triplet_hihat.length -1;i++){
+        hihatMatrix3.matrix[i][0] = parseInt(parsed_triplet_hihat[i]) 
+        hihatMatrix3.draw()
+        }
+    }
+     if(parsed_sixteen_hihat!== undefined ||parsed_sixteen_hihat !== null ||parsed_sixteen_hihat != undefined || parsed_sixteen_hihat != null){
+        for(var i=0; i<= parsed_sixteen_hihat.length -1;i++){
+        hihatMatrix4.matrix[i][0] = parseInt(parsed_sixteen_hihat[i]) 
+        hihatMatrix4.draw()
+        }
+    }
+     if(parsed_four_fx!== undefined ||parsed_four_fx !== null ||parsed_four_fx != undefined || parsed_four_fx != null){
+        for(var i=0; i<= parsed_four_fx.length -1;i++){
+        fxMatrix.matrix[i][0] = parseInt(parsed_four_fx[i]) 
+        fxMatrix.draw()
+        }
+    }
+     if(parsed_eight_fx!== undefined ||parsed_eight_fx !== null ||parsed_eight_fx != undefined || parsed_eight_fx != null){
+        for(var i=0; i<= parsed_eight_fx.length -1;i++){
+        fxMatrix2.matrix[i][0] = parseInt(parsed_eight_fx[i]) 
+        fxMatrix2.draw()
+        }
+    }
+     if(parsed_triplet_fx!== undefined ||parsed_triplet_fx !== null ||parsed_triplet_fx != undefined || parsed_triplet_fx != null){
+        for(var i=0; i<= parsed_triplet_fx.length -1;i++){
+        fxMatrix3.matrix[i][0] = parseInt(parsed_triplet_fx[i]) 
+        fxMatrix3.draw()
+        }
+    }
+     if(parsed_sixteen_fx!== undefined ||parsed_sixteen_fx !== null ||parsed_sixteen_fx != undefined || parsed_sixteen_fx != null){
+        for(var i=0; i<= parsed_sixteen_fx.length -1;i++){
+        fxMatrix4.matrix[i][0] = parseInt(parsed_sixteen_fx[i]) 
+        fxMatrix4.draw()
+        }
+    }
+ 
 }
+// window.onload= function (){
+    
+//     }
+    
+// }
 
 
 
--- public/js/engine/localStorage.js
+++ public/js/engine/localStorage.js
@@ -2,41 +2,84 @@
 
 $('#save').on("click", function(){
 
-var input = kickMatrix.matrix
-console.log(input)
-// localStorage.clear()
-localStorage.setItem("kick", input);
+let four_kick = kickMatrix.matrix
+let eight_kick = kickMatrix2.matrix
+let triplet_kick = kickMatrix3.matrix
+let sixteen_kick = kickMatrix4.matrix
 
-console.log(typeof localStorage.kick)
-matrixLoad()
+let four_snare = snareMatrix.matrix
+let eight_snare = snareMatrix2.matrix
+let triplet_snare = snareMatrix3.matrix
+let sixteen_snare = snareMatrix4.matrix
+
+let four_hihat = hihatMatrix.matrix
+let eight_hihat = hihatMatrix2.matrix
+let triplet_hihat = hihatMatrix3.matrix
+let sixteen_hihat = hihatMatrix4.matrix
+
+let four_fx = fxMatrix.matrix
+let eight_fx = fxMatrix2.matrix
+let triplet_fx = fxMatrix3.matrix
+let sixteen_fx = fxMatrix4.matrix
+
+// localStorage.clear()
+
+localStorage.setItem("four_kick", four_kick);
+localStorage.setItem("eight_kick", eight_kick);
+localStorage.setItem("triplet_kick", triplet_kick);
+localStorage.setItem("sixteen_kick", sixteen_kick);
+localStorage.setItem("four_snare", four_snare);
+localStorage.setItem("eight_snare", eight_snare);
+localStorage.setItem("triplet_snare", triplet_snare);
+localStorage.setItem("sixteen_snare", sixteen_snare);
+localStorage.setItem("four_hihat", four_hihat);
+localStorage.setItem("eight_hihat", eight_hihat);
+localStorage.setItem("triplet_hihat", triplet_hihat);
+localStorage.setItem("sixteen_hihat", sixteen_hihat);
+localStorage.setItem("four_fx", four_fx);
+localStorage.setItem("eight_fx", eight_fx);
+localStorage.setItem("triplet_fx", triplet_fx);
+localStorage.setItem("sixteen_fx", sixteen_fx);
+
+
+
 })
 
 
-function matrixLoad(){
-	let value = localStorage.getItem('kick');
-	let parsed =value.split(",")
-	console.log(parsed)
-	// parsed.forEach(function(key){
-	// 	parseInt(key)
-	// })
-	// console.log(parsed)
 
-	for(var i=0; i<= parsed.length -1;i++){
-		console.log(typeof parseInt(parsed[i]))
-		console.log(parseInt(parsed[i]))
-		kickMatrix2.matrix[i][0] = parseInt(parsed[i]) || 0
-        kickMatrix2.draw()
-	}
 
-}
+// (function ($) {
+//     // Run on page load
+//     window.onload = function() {
+//         var person_number = sessionStorage.getItem("person_number");
+//         var email = sessionStorage.getItem("email");
+
+//         if (person_number !== undefined || person_number !== null ||
+//             person_number != undefined || person_number != null){
+//             $('#person_number').val(person_number);
+//         }
+//         if (email !== undefined || email !== null ||
+//             email != undefined || email != null){
+//             $('#username').val(email);
+//         }
+//     }
+
+//     // Before refreshing the page, save the form data to sessionStorage
+//     window.onbeforeunload = function() {
+
+//         var person_number = $('#person_number').val();
+//         var email = $('#username').val();
+
+//         if (person_number !== undefined || person_number !== null ||
+//             person_number != undefined || person_number != null){
+//             sessionStorage.setItem("person_number", person_number);
+//         }
+//         if (email !== undefined || email !== null ||
+//             email != undefined || email != null){
+//             sessionStorage.setItem("email", email);
+//         }
+//     }
+// })(jQuery);
 	
-
-
-function filter(input){
-	input.forEach(kick,
-		function(){
-			console.log(input)
-		})
-}
 
 
